21dc463e5c95f3c833a1501346b279f50b306c0b
khansa86 <57402284+khansa86@users.noreply.github.com>
2020-02-18 20:37:40 +0500

    Update cnnface.py

--- cnnface.py
+++ cnnface.py
@@ -5,16 +5,8 @@ Created on Tue Feb 18 09:55:25 2020
 @author: hinaa
 """
 
-# Convolutional Neural Network
 
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
 
-# Installing Tensorflow
-# Install Tensorflow from the website: https://www.tensorflow.org/versions/r0.12/get_started/os_setup.html
-
-# Installing Keras
-# pip install --upgrade keras
 
 # Part 1 - Building the CNN
 
